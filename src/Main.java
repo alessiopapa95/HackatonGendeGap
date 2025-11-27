633e01133f4c845cfcca37544c7420b9588012ab
alessiopapa95 <136860541+alessiopapa95@users.noreply.github.com>
2025-11-27 10:08:58 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import javafx.application.Application;
 import dashboard.Dashboard;
 
-//Ciao Alessio come stai ?
-//Io sto bene
-
 public class Main {
     public static void main(String[] args) {
         Application.launch(Dashboard.class, args);
