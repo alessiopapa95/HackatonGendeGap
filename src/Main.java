5ac358921301339d3f2e39deb9b77d6dc226b28c
alessiopapa95 <136860541+alessiopapa95@users.noreply.github.com>
2025-11-27 09:58:23 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import javafx.application.Application;
 import dashboard.Dashboard;
 
+<<<<<<< Updated upstream
 //Ciao Alessio come stai ?
 //Io sto bene
+=======
+//We
+>>>>>>> Stashed changes
 
 public class Main {
     public static void main(String[] args) {
